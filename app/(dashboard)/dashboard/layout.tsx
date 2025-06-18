f872019dc6acd9da7a19552aa1b9f226a968c303
Menocas17 <rodolfomenocal7@gmail.com>
2025-06-18 16:24:15 -0600

    Finishing the responsive and fixinf little details

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -3,6 +3,12 @@ import SideNav from '@/components/dashboard/sidenav';
 import styles from '@/styles/dashboard/sidenav.module.css';
 import { SessionProvider } from 'next-auth/react';
 
+export const metadata = {
+  title: 'Profile Dashboard',
+  description:
+    'In this dashboard you will find all the options to manage your seller and user profile',
+};
+
 export default async function LoginLayout({
   children,
 }: {
